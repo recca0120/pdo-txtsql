87f42521b5b8385c5a1721b262a90cc6058690f9
recca0120 <recca0120@gmail.com>
2018-04-18 04:01:51 +0800

    update

--- src/PDO.php
+++ src/PDO.php
@@ -2,8 +2,69 @@
 
 namespace Recca0120\TxtSQL;
 
-class PDO
+use PDOException;
+
+class PDO extends \PDO
 {
+    /**
+     *
+     * The attributes for a lazy connection.
+     *
+     * @var array
+     *
+     */
+    protected $attributes = [];
+
+    /**
+     *
+     * The DSN for a lazy connection.
+     *
+     * @var string
+     *
+     */
+    protected $dsn;
+
+    /**
+     *
+     * PDO options for a lazy connection.
+     *
+     * @var array
+     *
+     */
+    protected $options = [];
+
+    /**
+     *
+     * The username for a lazy connection.
+     *
+     * @var string
+     *
+     */
+    protected $username;
+
+    /**
+     *
+     * The password for a lazy connection.
+     *
+     * @var string
+     *
+     */
+    protected $password;
+
+    /**
+     * Undocumented variable
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Undocumented variable
+     *
+     * @var string
+     */
+    protected $dbname;
+
     /**
      *
      * Constructor.
@@ -20,7 +81,77 @@ class PDO
      *
      */
 
-    public function __construct($dsn, $username = null, $passwd = null, $options = [])
+    public function __construct($dsn, $username = 'root', $passwd = null, $options = [])
     {
+        // if no error mode is specified, use exceptions
+        if (! isset($options[PDO::ATTR_ERRMODE])) {
+            $options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+        }
+
+        $this->connect($dsn, $username, $passwd, $options);
+    }
+
+    private function connect($dsn, $username, $passwd, $options)
+    {
+        $this->dsn = $dsn;
+        $this->username = $username;
+        $this->password = $passwd;
+        $this->options = $options;
+
+        $parsedDsn = $this->parseDsn($this->dsn, ['dbname', 'path', 'file']);
+        $this->dbname = $parsedDsn['dbname'];
+        $this->path = isset($parsedDsn['path']) ? $parsedDsn['path'] : $parsedDsn['file'];
+
+        if ($this->existsFile($this->dbname) === false) {
+            throw new PDOException(
+                sprintf("SQLSTATE[HY000] [1049] Unknown database '%s'", $this->dbname),
+                1049
+            );
+        }
+
+
+        $users = $this->readFile('txtsql/txtsql.MYI');
+        $username = strtolower($this->username);
+        if (empty($users[$username]) === true || $users[$username] !== md5($this->password)) {
+            throw new PDOException(
+                sprintf("SQLSTATE[HY000] [1045] Access denied for user '%s'@'%s' (using password: YES)", $this->username, $this->path),
+                1045
+            );
+        }
+
+        return true;
+    }
+
+    private function readFile($file)
+    {
+        return unserialize(file_get_contents(sprintf('%s/%s', $this->path, $file)));
+    }
+
+    private function existsFile($file)
+    {
+        $file = sprintf('%s/%s', $this->path, $file);
+
+        return file_exists($file) === true && is_writable($file) === true;
+    }
+
+    private function parseDsn($dsn, $params)
+    {
+        if (strpos($dsn, ':') !== false) {
+            $driver = substr($dsn, 0, strpos($dsn, ':'));
+            $vars = substr($dsn, strpos($dsn, ':') + 1);
+
+            $returnParams = [
+                'driver' => $driver,
+            ];
+            foreach (explode(';', $vars) as $var) {
+                $param = explode('=', $var);
+                if (in_array($param[0], $params)) {
+                    $returnParams[$param[0]] = $param[1];
+                }
+            }
+            return $returnParams;
+        }
+
+        return [];
     }
 }
--- tests/old/ConnectionTest.php
+++ tests/old/ConnectionTest.php
@@ -3,14 +3,21 @@
 namespace Recca0120\Tests\TxtSQL\Old;
 
 use txtSQL;
+use PDOException;
+use Recca0120\TxtSQL\PDO;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
+
+use Exception;
 
 class ConnectionTest extends TestCase
 {
     protected function setUp()
     {
-        $this->folder = __DIR__.'/../../data';
+        $this->path = realpath(__DIR__.'/../../txtSQL/v2.2/data');
+        $this->username = 'root';
+        $this->passwd = '';
+        $this->dbname = 'test';
     }
 
     protected function tearDown()
@@ -19,9 +26,33 @@ class ConnectionTest extends TestCase
         m::close();
     }
 
+    public function test_user_validate_fail()
+    {
+        try {
+            $pdo = new PDO(sprintf('txtsql:dbname=txtsql;path=%s', $this->path), 'root', 'root');
+        } catch (PDOException $e) {
+            $this->assertInstanceOf('PDOException', $e);
+            $this->assertSame(sprintf("SQLSTATE[HY000] [1045] Access denied for user '%s'@'%s' (using password: YES)", 'root', $this->path), $e->getMessage());
+            $this->assertSame(1045, $e->getCode());
+        }
+    }
+
+    public function test_database_not_exists()
+    {
+        try {
+            $pdo = new PDO(sprintf('txtsql:dbname=fake-test;path=%s', $this->path), 'root', 'root');
+        } catch (PDOException $e) {
+            $this->assertInstanceOf('PDOException', $e);
+            $this->assertSame(sprintf("SQLSTATE[HY000] [1049] Unknown database '%s'", 'fake-test'), $e->getMessage());
+            $this->assertSame(1049, $e->getCode());
+        }
+    }
+
     public function test_connection()
     {
-        $connection = new txtSQL($this->folder);
-        $this->assertTrue($connection->connect('root', ''));
+        $connection = new txtSQL($this->path);
+        $this->assertTrue($connection->connect($this->username, $this->passwd));
+
+        $pdo = new PDO(sprintf('txtsql:dbname=%s;file=%s', $this->dbname, $this->path), $this->username, $this->passwd);
     }
 }
